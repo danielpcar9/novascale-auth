edd8a3c4c8f82409066e51afe1991d10493d6969
Daniel Palomeque <eldanypalomo@gmail.com>
2026-01-15 21:30:56 -0600

    feat: add user model with pydantic v2 validation

---
+++ models.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel, EmailStr, Field, field_validator, ValidationError
+from typing import Annotated
+
+NameString = Annotated[str, Field(min_length=1, max_length=100)]
+
+
+class User(BaseModel):
+    name: NameString
+    email: EmailStr
+    age: int = Field(ge=0, le=120)
+    username: str = Field(min_length=5)
+
+    @field_validator("name")
+    @classmethod
+    def name_must_be_alphabetic(cls, v: str) -> str:
+        cleaned_name = v.strip()
+        if not all(part.isalpha() for part in cleaned_name.split()):
+            raise ValidationError("Name must have alphabetical characters and spaces")
+        return cleaned_name.title()
+
+    @field_validator("username")
+    @classmethod
+    def validate_username(cls, v: str) -> str:
+        v = v.strip().lower()
+        if "admin" in v:
+            raise ValidationError("For safety username cannot contain the word 'admin")
+
+        return v
+
+
+if __name__ == "__main__":
+    try:
+        # Prueba exitosa con todo combinado
+        user = User(
+            name="  ana garcía  ",
+            email="ANA@example.com",
+            age=30,
+            username="  AnaDev2026  ",
+        )
+        print("✅ TODO PERFECTO:")
+        print(f"Nombre: {user.name}")  # Ana García
+        print(f"Username: {user.username}")  # anadev2026
+
+    except Exception as e:
+        print(f"❌ Error de validación:\n{e}")
